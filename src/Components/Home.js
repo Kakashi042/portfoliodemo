a59894ccb2f8cf760630eec76771123b450a9995
Blurry Face <kakashi.code.take@gmail.com>
2023-04-17 09:44:38 +0530

    Commiting the changes made to several files

---
+++ src/Components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div id='home' className='section'>Home</div>
+  )
+}
+
+export default Home
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-scroll'
+import '../Styles/Navbar.css'
 
 const Navbar = () => {
+    const[clicked, setClicked] = useState(false);
+
+    const handle = () =>{
+        if(clicked === false)
+        setClicked(true);
+        else
+        setClicked(false);
+    }
+
   return (
-    <nav className='navbar'>
-       <Link to='home'>Prajwal</Link>
-        <ul>
+    <nav className='navbar' >
+       <Link to='home' spy={true} smooth={true} offset={-120} duration={500}>Prajwal</Link>
+        <ul id='links' className={clicked?'#links active':'#links'}>
             <li>
-                <Link to='about'>About</Link>
+                <Link to='about' spy={true} smooth={true} offset={-120} duration={500}>About</Link>
             </li>
             <li>
-                <Link to='work'>Works</Link>
+                <Link to='work' spy={true} smooth={true} offset={-120} duration={500}>Works</Link>
             </li>
             <li>
-                <Link to='contact'>Contact</Link>
+                <Link to='contact' spy={true} smooth={true} offset={-120} duration={500}>Contact</Link>
             </li>
         </ul>
+        <div className='icons' onClick={handle}>
+            <i className={clicked?'fas fa-times':'fas fa-bars'}></i>
+        </div>
     </nav>
   )
 }
